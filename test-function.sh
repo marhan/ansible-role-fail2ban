c597ea76e57abcb4513146701efffe2c8c0f6508
Markus Hanses <me@markushanses.de>
2018-05-26 20:52:49 +0200

    No tty support at buildserver.de.

--- test-function.sh
+++ test-function.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/tmp/$(basename "${PWD}"):ro -v /var/run/docker.sock:/var/run/docker.sock -w /tmp/$(basename "${PWD}") retr0h/molecule:latest sudo molecule test
+docker run --rm -i -v $(pwd):/tmp/$(basename "${PWD}"):ro -v /var/run/docker.sock:/var/run/docker.sock -w /tmp/$(basename "${PWD}") retr0h/molecule:latest sudo molecule test
