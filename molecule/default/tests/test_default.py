cc30a4b823f976e0b16315f0db5847addaf61c16
Markus Hanses <me@markushanses.de>
2018-05-30 21:18:01 +0200

    Test Playbook with variables.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -21,10 +21,10 @@ def test_jail_local(host):
     assert subject.exists
     assert subject.user == 'root'
     assert subject.group == 'root'
-    assert b'destemail = recipent@testdomain.com' in subject.content
+    assert b'destemail = another_recipent@anotherdomain.com' in subject.content
 
 
 @pytest.mark.parametrize("file_name", ["mail.conf", "mail-whois-lines.conf", "mail-buffered.conf", "mail-whois.conf"])
 def test_action_d_conf(host, file_name):
     content = host.file('/etc/fail2ban/action.d/' + file_name).content
-    assert b'dest = recipent@testdomain.com' in content
+    assert b'dest = another_recipent@anotherdomain.com' in content
