3365d0e9eba22d5c9fff955f8a23da566a616d8b
Markus Hanses <me@markushanses.de>
2018-05-30 22:39:17 +0200

    Special molecule scenario for test with variables.

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,11 +1,24 @@
 pipeline {
   agent { label 'molecule' }
 
+  environment {
+    ANSIBLE_ROLE_NAME = "ansible-role-fail2ban"
+    DOCKER_COMMAND_PART_1 = "docker run --rm -i -v \$(pwd):/tmp/${ANSIBLE_ROLE_NAME}:ro -v /var/run/docker.sock:/var/run/docker.sock -w /tmp/${ANSIBLE_ROLE_NAME} retr0h/molecule:latest"
+  }
+
   stages {
-    stage('Test') {
+
+    stage('Test (default)') {
       steps {
-        sh "./tests/test_via_docker.sh"
+        sh "${DOCKER_COMMAND_PART_1} sudo molecule test -s default"
       }
     }
+
+    stage('Test (variables)') {
+      steps {
+        sh "${DOCKER_COMMAND_PART_1} sudo molecule test -s variables"
+      }
+    }
+
   }
 }
--- molecule/variables/tests/test_default.py
+++ molecule/variables/tests/test_default.py
@@ -7,24 +7,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize("name,version", [
-    ("fail2ban", "0.9"),
-])
-def test_packages(host, name, version):
-    subject = host.package(name)
-    assert subject.is_installed
-    assert subject.version.startswith(version)
-
-
 def test_jail_local(host):
     subject = host.file('/etc/fail2ban/jail.local')
     assert subject.exists
     assert subject.user == 'root'
     assert subject.group == 'root'
-    assert b'destemail = recipent@testdomain.com' in subject.content
+    assert b'destemail = another_recipent@anotherdomain.com' in subject.content
 
 
 @pytest.mark.parametrize("file_name", ["mail.conf", "mail-whois-lines.conf", "mail-buffered.conf", "mail-whois.conf"])
 def test_action_d_conf(host, file_name):
     content = host.file('/etc/fail2ban/action.d/' + file_name).content
-    assert b'dest = recipent@testdomain.com' in content
+    assert b'dest = another_recipent@anotherdomain.com' in content
